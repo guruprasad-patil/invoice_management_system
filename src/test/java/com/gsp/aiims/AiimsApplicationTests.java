5ad90d87cc230beac12eb51bdc69bb554de41c6c
dev-guruprasad <guruprasadupwork@gmail.com>
2026-05-24 21:29:03 +0530

    initial commit

---
+++ src/test/java/com/gsp/aiims/AiimsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gsp.aiims;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AiimsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
